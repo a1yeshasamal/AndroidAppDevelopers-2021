d84e9d11c31ae1a45a97a44a5f0e554ec88de1d0
a1yeshasamal <78255915+a1yeshasamal@users.noreply.github.com>
2021-04-12 21:23:12 +0530

    First assignment for Digilock

---
+++ app/src/main/java/com/example/mojo/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.mojo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
+Output:
